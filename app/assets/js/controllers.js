0902e2323554231db2531d37119739da9a30e6ac
Harley Glossop <harleyglossop@gmail.com>
2016-01-03 19:28:12 +0000

    add current/target temp display

--- app/assets/js/controllers.js
+++ app/assets/js/controllers.js
@@ -1,5 +1,15 @@
 var SousVideControllers = angular.module('SousVideControllers', []);
 
 SousVideControllers.controller('indexController', function($scope) {
-	$scope.message = "Hello, world!";
+	$scope.current = 0;
+	$scope.target = 0;
+	$scope.error = $scope.current - $scope.target
+	if (Math.abs($scope.error) <= 0.1) {
+		$scope.currentColor = "success";
+	} else if (Math.abs($scope.error) <= 1.0) {
+		$scope.currentColor = "warning";
+	} else {
+		$scope.currentColor = "danger";
+	}
+	
 });
