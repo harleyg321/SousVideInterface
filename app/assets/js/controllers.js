60139a9f4a6fcfd261627e0a116187dbca92eb1b
Harley Glossop <harleyglossop@gmail.com>
2016-01-05 22:34:31 +0000

    initial selection menu

--- app/assets/js/controllers.js
+++ app/assets/js/controllers.js
@@ -11,5 +11,65 @@ SousVideControllers.controller('indexController', function($scope) {
 	} else {
 		$scope.currentColor = "danger";
 	}
+
+	$scope.userMeat = new Object();
+	$scope.userColor = new Object();
+	$scope.userTemp = 50;
+
+	$scope.meats = [
+		{
+			type: "Beef",
+			colors: [
+				{
+					color: "Blue",
+					min: 20,
+					max: 30
+				}
+			]
+		}
+	];
+
+	/*$scope.meats = ["Beef", "Pork", "Chicken"];
 	
+	$scope.colors = [
+		{
+			level: "Blue",
+			meat: "Beef"
+		},
+		{
+			level: "Rare",
+			meat: "Beef"
+		},
+		{
+			level: "Medium-Rare",
+			meat: "Beef"
+		},
+		{
+			level: "Medium",
+			meat: "Beef"
+		},
+		{
+			level: "Medium-Well",
+			meat: "Beef"
+		},
+		{
+			level: "Well Done",
+			meat: "Beef"
+		}
+	];
+
+	$scope.temps = [
+		{
+			min: 46, 
+			max: 49,
+			meat: "Beef",
+			color: "Blue"
+		},
+		{
+			min: 52,
+			max: 55,
+			meat: "Beef",
+			color: "Rare"
+		}
+	];*/
 });
