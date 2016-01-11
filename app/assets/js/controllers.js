02e20428e91688ea9453ae82b2693bc86e50d459
Harley Glossop <harleyglossop@gmail.com>
2016-01-11 01:31:30 +0000

    receive temperature over serial connection, includin support for multiple thermometers

--- app/assets/js/controllers.js
+++ app/assets/js/controllers.js
@@ -3,15 +3,11 @@ var SousVideControllers = angular.module('SousVideControllers', []);
 SousVideControllers.controller('indexController', function($scope) {
 	$scope.current = 0;
 	$scope.target = 0;
-	$scope.error = $scope.current - $scope.target;
+	$scope.error = 0;
+	$scope.variance = 0;
+	$scope.multiple = false;
+	$scope.currentColor = "success";
 
-	if (Math.abs($scope.error) <= 0.1) {
-		$scope.currentColor = "success";
-	} else if (Math.abs($scope.error) <= 1.0) {
-		$scope.currentColor = "warning";
-	} else {
-		$scope.currentColor = "danger";
-	}
 
 	$scope.userMeat = new Object();
 	$scope.userColor = new Object();
@@ -58,6 +54,30 @@ SousVideControllers.controller('indexController', function($scope) {
 	$scope.$watch('userColor', function(newValue, oldValue) {
 		// on initial page load newValue is undefined, so set to existing value
 		$scope.userTemp = Math.round((newValue.min + newValue.max)/2) || $scope.userTemp;
+	});
+
+	var socket = io();
+
+	socket.on('temperature', function(data) {
+		$scope.$apply(function() {
+				var temp_str = data.split(",");
+				var temp = 0;
+				temp_str.forEach(function(entry) {
+					temp += +entry;
+				});
+				temp /= temp_str.length;
+				$scope.current = temp;
+				$scope.variance = Math.abs(Math.max.apply(Math, temp_str) - Math.min.apply(Math, temp_str));
+				$scope.multiple = temp_str.length > 1;
+				$scope.error = $scope.current - $scope.target;
+					if (Math.abs($scope.error) <= 0.1) {
+						$scope.currentColor = "success";
+					} else if (Math.abs($scope.error) <= 1.0) {
+						$scope.currentColor = "warning";
+					} else {
+						$scope.currentColor = "danger";
+					}
+		});
 	});
 	
 });
