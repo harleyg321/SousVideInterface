ef7fe74161cea33da4e82e880b4e0ce4a80e2d4f
Harley Glossop <harleyglossop@gmail.com>
2016-01-12 15:49:50 +0000

    graph added, functioning with static data

--- app/assets/js/controllers.js
+++ app/assets/js/controllers.js
@@ -1,6 +1,6 @@
 var SousVideControllers = angular.module('SousVideControllers', []);
 
-SousVideControllers.controller('indexController', function($scope) {
+SousVideControllers.controller('indexController', function($scope, $window) {
 	$scope.current = 0;
 	$scope.target = 0;
 	$scope.error = 0;
@@ -81,5 +81,119 @@ SousVideControllers.controller('indexController', function($scope) {
 			}
 		});
 	});
+
+	var w = $window.innerWidth,
+		h = $window.innerHeight,
+		margin = {top: 10, right: 0, bottom: 100, left: 0},
+	    margin2 = {top: h-90, right: 0, bottom: 0, left: 0},
+	    width = w - margin.left - margin.right,
+	    height = h - margin.top - margin.bottom,
+	    height2 = h - margin2.top - margin2.bottom;
+
+	var chartdata;
+
+	var x = d3.scale.linear().range([0, width]),
+	    x2 = d3.scale.linear().range([0, width]),
+	    y = d3.scale.linear().range([height, 0]),
+	    y2 = d3.scale.linear().range([height2, 0]);
+
+	var brush = d3.svg.brush()
+	    .x(x2)
+	    .on("brush", brushed);
+
+	var current1 = d3.svg.line()
+	    .interpolate("monotone")
+	    .x(function(d) { return x(d.i); })
+	    .y(function(d) { return y(d.current); });
+
+	var current2 = d3.svg.line()
+	    .interpolate("monotone")
+	    .x(function(d) { return x2(d.i); })
+	    .y(function(d) { return y2(d.current); });
+
+	var target1 = d3.svg.line()
+	    .interpolate("monotone")
+	    .x(function(d) { return x(d.i); })
+	    .y(function(d) { return y(d.target); });
+
+	var target2 = d3.svg.line()
+	    .interpolate("monotone")
+	    .x(function(d) { return x2(d.i); })
+	    .y(function(d) { return y2(d.target); });
+
+	var svg = d3.select("#chart")
+		.append("svg")
+		.attr("preserveAspectRatio", "none")
+		.attr("viewBox", "0 0 " + (width + margin.left + margin.right) + " " + (height + margin.top + margin.bottom));
+
+	svg.append("defs").append("clipPath")
+	    .attr("id", "clip")
+	  .append("rect")
+	    .attr("width", width)
+	    .attr("height", height);
+
+	var focus = svg.append("g")
+	    .attr("class", "focus")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	var context = svg.append("g")
+	    .attr("class", "context")
+	    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+	d3.csv("/assets/data.csv", type, function(error, data) {
+	  x.domain(d3.extent(data.map(function(d) { return d.i; })));
+	  y.domain([d3.min(data.map(function(d) { return Math.min(d.current, d.target); })),
+	  			d3.max(data.map(function(d) { return Math.max(d.current, d.target); }))]);
+	  x2.domain(x.domain());
+	  y2.domain(y.domain());
+
+	  chartdata = data;
+
+	  focus.append("path")
+	      .datum(data)
+	      .attr("class", "current-line")
+	      .attr("d", current1);
+
+	  context.append("path")
+	      .datum(data)
+	      .attr("class", "current-line")
+	      .attr("d", current2);
+
+	  focus.append("path")
+	      .datum(data)
+	      .attr("class", "target-line")
+	      .attr("d", target1);
+
+	  context.append("path")
+	      .datum(data)
+	      .attr("class", "target-line")
+	      .attr("d", target2);
+
+	  context.append("g")
+	      .attr("class", "x brush")
+	      .call(brush)
+	    .selectAll("rect")
+	      .attr("y", -6)
+	      .attr("height", height2 + 7);
+	});
+
+	function brushed() {
+	  x.domain(brush.empty() ? x2.domain() : brush.extent());
+	  var dataFiltered = chartdata.filter(function(d, i) {
+	      if ( (d.i >= (x.domain()[0])-1) && (d.i <= (x.domain()[1])+1) ) {
+	        return d;
+	      }
+      });
+      y.domain([d3.min(dataFiltered.map(function(d) { return Math.min(d.current,d.target); })),
+      			d3.max(dataFiltered.map(function(d) { return Math.max(d.current,d.target); }))]);
+	  focus.select(".current-line").attr("d", current1);
+	  focus.select(".target-line").attr("d", target1);
+	}
+
+	function type(d) {
+	  d.i = +d.i;
+	  d.current = +d.current;
+	  return d;
+	}
 	
 });
