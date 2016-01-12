4a587c42ab18a4fa7b70dcc9036e7245070fea4e
Harley Glossop <harleyglossop@gmail.com>
2016-01-12 00:38:46 +0000

    change . for ng-change

--- app/assets/js/controllers.js
+++ app/assets/js/controllers.js
@@ -51,32 +51,34 @@ SousVideControllers.controller('indexController', function($scope) {
 		}
 	];
 
-	$scope.$watch('userColor', function(newValue, oldValue) {
-		// on initial page load newValue is undefined, so set to existing value
-		$scope.userTemp = Math.round((newValue.min + newValue.max)/2) || $scope.userTemp;
-	});
+	$scope.userColorChange = function() {
+		$scope.userTemp = Math.round(($scope.userColor.min + $scope.userColor.max)/2);
+	};
 
 	var socket = io();
 
 	socket.on('temperature', function(data) {
 		$scope.$apply(function() {
-				var temp_str = data.split(",");
-				var temp = 0;
-				temp_str.forEach(function(entry) {
-					temp += +entry;
-				});
-				temp /= temp_str.length;
-				$scope.current = temp;
-				$scope.variance = Math.abs(Math.max.apply(Math, temp_str) - Math.min.apply(Math, temp_str));
-				$scope.multiple = temp_str.length > 1;
-				$scope.error = $scope.current - $scope.target;
-					if (Math.abs($scope.error) <= 0.1) {
-						$scope.currentColor = "success";
-					} else if (Math.abs($scope.error) <= 1.0) {
-						$scope.currentColor = "warning";
-					} else {
-						$scope.currentColor = "danger";
-					}
+			var temp_str = data.split(",");
+			var temp = 0;
+
+			temp_str.forEach(function(entry) {
+				temp += +entry;
+			});
+			temp /= temp_str.length;
+
+			$scope.current = temp;
+			$scope.variance = Math.abs(Math.max.apply(Math, temp_str) - Math.min.apply(Math, temp_str));
+			$scope.multiple = temp_str.length > 1;
+			$scope.error = $scope.current - $scope.target;
+
+			if (Math.abs($scope.error) <= 0.1) {
+				$scope.currentColor = "success";
+			} else if (Math.abs($scope.error) <= 1.0) {
+				$scope.currentColor = "warning";
+			} else {
+				$scope.currentColor = "danger";
+			}
 		});
 	});
 	
